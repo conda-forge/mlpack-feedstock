22f5c87e2d39a2d053a94f99ecf8204d889bc7e5
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-01-07 16:51:37 -0200

    Update mlpack

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,12 @@ cmake \
 	-DBUILD_PYTHON_BINDINGS=ON \
 	-DBUILD_CLI_EXECUTABLES=ON \
 	-DBUILD_SHARED_LIBRARIES=ON \
-	-DFORCE_CXX11=OFF \
+	-DFORCE_CXX11=ON \
 	-DBUILD_TESTS=OFF \
-	../
+        -DBOOST_INCLUDEDIR="${PREFIX}/include" \
+        -DBOOST_LIBRARYDIR="${PREFIX}/lib" \
+        -DBOOST_ROOT="${PREFIX}" \
+	..
 
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} VERBOSE=1
 make install
