041bc96f1e2c790029660f32f61682476343bdcd
himanshupathak21061998 <hpathak336@gmail.com>
2020-05-09 21:34:41 +0530

    Applying some suggestion

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ cd build
 cmake -GNinja ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -DCMAKE_BUILD_TYPE="Debug" ^
+      -DCMAKE_BUILD_TYPE="Release" ^
       -DBUILD_PYTHON_BINDINGS=ON ^
       -DBUILD_CLI_EXECUTABLES=ON ^
       -DBUILD_SHARED_LIBS=OFF ^
@@ -17,13 +17,13 @@ cmake -GNinja ^
       -DCMAKE_CXX_COMPILER=cl.exe ^
       -DCMAKE_C_COMPILER=cl.exe ^
       -DBoost_INCLUDE_DIRS=%LIBRARY_PREFIX%/include ^
-      -DCMAKE_CXX_FLAGS="-DBOOST_ALL_NO_LIB=1" ^
+      -DBoost_USE_STATIC_LIBS=ON
       -DENSMALLEN_INCLUDE_DIR=%LIBRARY_PREFIX%/include ^
       -DARMADILLO_INCLUDE_DIR=%LIBRARY_PREFIX%/include ^
-      -DARMADILLO_LIBRARIES=%LIBRARY_PREFIX%/lib/armadillo.lib ^
+      -DARMADILLO_LIBRARIES="%LIBRARY_PREFIX%/lib/armadillo.lib;%LIBRARY_PREFIX%/lib/openblas.lib" ^
       -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=true ^
       -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=100000 ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       ..
 
-cmake --build . --target install --config Debug
+cmake --build . --target install --config Release
